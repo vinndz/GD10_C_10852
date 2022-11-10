5e22084d5e3eaf4219d66f865119c5c1a77a86d5
vinndz <jeremykev186@gmail.com>
2022-11-10 15:44:23 +0700

    commit

---
+++ app/Models/Modelmahasiswa.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Modelmahasiswa extends Model
+{
+    protected $table = 'mahasiswa';
+    protected $primaryKey = 'mhsnobp';
+    protected $allowedFields = [
+        'mhsnobp','mhsnama','mhsalamat','prodinama','mhstgllhr'
+    ];
+}
