a69fc121111bda07baaa559fc0ed87df2240786a
vinndz <jeremykev186@gmail.com>
2022-11-10 15:35:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gd10_c_10852/ResponseCreate.kt
@@ -0,0 +1,9 @@
+package com.example.gd10_c_10852
+
+import com.google.gson.annotations.SerializedName
+
+class ResponseCreate (
+    @SerializedName("status") val stt:Int,
+    @SerializedName("error") val e:Boolean,
+    @SerializedName("message") val pesan:String,
+)
---
+++ app/src/main/java/com/example/gd10_c_10852/ResponseDataMahasiswa.kt
@@ -0,0 +1,9 @@
+package com.example.gd10_c_10852
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>
+    )
